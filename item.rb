76df436eecc0ea26e6159ffc2a0a14208d37e8e3
Sanborn <sanbornh@rogers.com>
2014-03-28 08:19:59 -0400

    Added todo list, item class updates

--- item.rb
+++ item.rb
@@ -3,15 +3,10 @@ class Item
 	attr_accessor :price, :name, :import_tax, :reg_exempt
 
 	def initialize(attributes)
-		@price = attributes[0].to_f
-		@name = attributes[1]
-		@import_tax = attributes[2]
-		@reg_tax = attributes[3]
+		@price = attributes[:price]
+		@object = attributes[:object]
+		@import_tax = attributes[:imported]
+		@reg_tax = attributes[:basic_tax_exempt]
 	end
 end
 
-book = Item.new(12.95, "charles", false, true)
-puts book.price
-puts book.name
-puts book.imported
-puts book.tax_exempt
